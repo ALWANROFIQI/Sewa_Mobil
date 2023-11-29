e7708f0b5b36400100fbe77ac6e76c205d6d7755
ALWANROFIQI <99782385+ALWANROFIQI@users.noreply.github.com>
2023-11-29 20:13:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route Halaman home
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('homepage');
+
+//Route Halaman produk
 Route::get('produk', [\App\Http\Controllers\HomeController::class, 'produk'])->name('produk');
+
+//Route Halaman detail
 Route::get('detail/{car:slug}', [\App\Http\Controllers\HomeController::class, 'detail'])->name('detail');
+
+//Route Halaman contact
 Route::get('contact', [\App\Http\Controllers\HomeController::class, 'contact'])->name('contact');
 Route::post('contact', [\App\Http\Controllers\HomeController::class, 'contactStore'])->name('contact.store');
+//Route Halaman about
 Route::get('about', [\App\Http\Controllers\HomeController::class, 'about'])->name('about');
 
+//Route Halaman dasboard admin
 Route::get('admin/dashboard', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard.index')->middleware('is_admin');
+
+//Route Halaman daftar mobil admin
 Route::resource('admin/cars', \App\Http\Controllers\Admin\CarController::class);
+//Route Halaman update mobil
 Route::put('admin/cars/update-image/{id}', [\App\Http\Controllers\Admin\CarController::class, 'updateImage'])->name('admin.cars.updateImage');
 
+//Route Halaman daftar pesan admin
 Route::get('admin/messages', [\App\Http\Controllers\Admin\MessageController::class, 'index'] )->name('admin.messages.index');
+//Route Halaman hapus pesan 
 Route::delete('admin/messages/{message}', [\App\Http\Controllers\Admin\MessageController::class, 'destroy'] )->name('admin.messages.destroy');
 Auth::routes();
 
