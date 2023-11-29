d2524f8a91da880e95cfee5214e2f358c041bd5b
ALWANROFIQI <99782385+ALWANROFIQI@users.noreply.github.com>
2023-11-29 20:16:41 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,13 +9,15 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
+    //Menampilkan halaman home 
     public function index()
     {
         $cars = Car::latest()->get();
 
         return view('frontend.homepage');
     }
+
+    //Menampilkan halaman produk
     public function produk()
     {
         $cars = Car::latest()->get();
@@ -23,11 +25,12 @@ class HomeController extends Controller
         return view('frontend.produk', compact('cars'));
     }
 
+    //Menampilkan halaman contact
     public function contact()
     {
         return view('frontend.contact');
     }
-
+    //Menampilkan halaman isi contact
     public function contactStore(Request $request)
     {
         $data = $request->validate([
@@ -38,17 +41,19 @@ class HomeController extends Controller
         ]);
          
         ModelsMessage :: create($data);
-
+        
+        //information message
         return redirect()->back()->with([
             'message' => 'pesan anda berhasil di kirim',
             'alert-type' =>'success'
         ]);
     }
+    //Menampilkan halaman about
     public function about()
     {
         return view('frontend.about');
     }
-
+    //Menampilkan halaman detail
     public function detail(Car $car)
     {
         return view('frontend.detail', compact('car'));
